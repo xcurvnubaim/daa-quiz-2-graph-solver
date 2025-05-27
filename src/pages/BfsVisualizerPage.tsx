afbcfb1c935c81f38b6244d657c5fafdc645a6e0
fawwasaldy <fawwas.aldy2111@gmail.com>
2025-05-27 09:09:28 +0700

    feat: add BFS visualizer page and update navigation links

---
+++ src/pages/BfsVisualizerPage.tsx
@@ -0,0 +1,9 @@
+// src/pages/BfsVisualizerPage.tsx
+import React from 'react';
+import BfsVisualizer from '../components/BfsVisualizer';
+
+const BfsVisualizerPage: React.FC = () => {
+    return <BfsVisualizer />;
+};
+
+export default BfsVisualizerPage;
